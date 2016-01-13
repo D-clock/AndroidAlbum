e215c28ba076d8d1eaf5eb22a53cd199ee18fcf2
clock <108499165clock@gmail.com>
2016-01-14 01:07:02 +0800

    更新一下获取图片路径的思路

--- src/main/java/com/clock/album/activity/SystemAlbumActivity.java
+++ src/main/java/com/clock/album/activity/SystemAlbumActivity.java
@@ -1,20 +1,10 @@
 package com.clock.album.activity;
 
-import android.content.res.Resources;
-import android.os.AsyncTask;
-import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.clock.album.R;
-
-import java.io.File;
-import java.io.FilenameFilter;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Vector;
 
 /**
  * 系统相册页面
@@ -29,50 +19,20 @@ public class SystemAlbumActivity extends AppCompatActivity implements View.OnCli
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_system_album);
 
-        findViewById(R.id.btn_method_01).setOnClickListener(this);
+        findViewById(R.id.btn_get_path).setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
         int viewId = v.getId();
-        if (viewId == R.id.btn_method_01) {
-            //http://stackoverflow.com/questions/10381270/how-to-get-all-image-files-available-in-sdcard-in-android
+        if (viewId == R.id.btn_get_path) {
+            //http://blog.csdn.net/xiaanming/article/details/18730223
+            //http://developer.android.com/reference/android/provider/MediaStore.Images.Media.html
+            //http://developer.android.com/reference/android/app/LoaderManager.html
+            //http://developer.android.com/reference/android/content/ContentProvider.html
+
         }
     }
 
-    private FilenameFilter[] getFilenameFilter() {
-        String[] imageTypes = getResources().getStringArray(R.array.imageType);
-        FilenameFilter[] filenameFilters = new FilenameFilter[imageTypes.length];
-        for (int imageTypePos = 0; imageTypePos < imageTypes.length; imageTypePos++) {
-            final String imageType = imageTypes[imageTypePos];
-            filenameFilters[imageTypePos] = new FilenameFilter() {
-                @Override
-                public boolean accept(File dir, String filename) {
-                    return filename.endsWith("." + imageType);
-                }
-            };
-        }
-        return filenameFilters;
-    }
-
-    private Collection<File> listFiles(File directory, FilenameFilter[] filter) {
-        Vector<File> fileVector = new Vector<File>();
-        File[] fileEntries = directory.listFiles();
-        if (fileEntries != null) {
-            for (File fileEntry : fileEntries) {
-
-                for (FilenameFilter filenameFilter : filter) {
-                    if (filenameFilter.accept(directory, fileEntry.getName())) {
-                        fileVector.add(fileEntry);
-                    }
-                }
-
-                if (fileEntry.isDirectory()) {
-                    fileVector.addAll(listFiles(fileEntry, filter));
-                }
-            }
-        }
-        return fileVector;
-    }
 }
